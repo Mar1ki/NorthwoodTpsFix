ce6ec17efb204c513a56cc7a116f40a9c2cda06e
Mariki <Mariki@DESKTOP-FPK6O16>
2024-10-25 19:09:45 +0300

    Добавьте файлы проекта.

---
+++ NorthwoodTpsFix/Patch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VoiceChat.Networking;
+using Exiled.API.Features.Pools;
+using static HarmonyLib.AccessTools;
+namespace NorthwoodTpsFix
+{
+    [HarmonyPatch(typeof(VoiceTransceiver), nameof(VoiceTransceiver.ServerReceiveMessage))]
+    public static class Patch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Call && instruction.operand == Method(typeof(UnityEngine.Mathf), nameof(Mathf.Abs), new Type[] { typeof(float) })) + 11;
+            newInstructions.InsertRange(index, new CodeInstruction[]
+            {
+                new CodeInstruction(OpCodes.Pop),
+                new CodeInstruction(OpCodes.Ldc_I4_S, 480),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+}
---
+++ NorthwoodTpsFix/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Features;
+using PlayerRoles.Spectating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwoodTpsFix
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "Mariki";
+        public override string Name => "TpsFix";
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+    }
+}
